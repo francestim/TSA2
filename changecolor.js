2abadce0497fbd1ed77ef8e77548b4e29e1505bd
francestim <151459060+francestim@users.noreply.github.com>
2024-02-04 17:59:45 +0700

    Add files via upload

---
+++ changecolor.js
@@ -0,0 +1,69 @@
+document.getElementById('mon').addEventListener('click', function() {
+    let box = document.getElementById('box-ani');
+    box.style.backgroundColor = 'white';
+    box.style.border = '10px solid purple';
+    box.style.display= 'block';
+    setTimeout(function() {
+      box.style.transform = 'translateY(250px)';
+    }, 100);
+  });
+
+document.getElementById('tues').addEventListener('click', function() {
+    let box = document.getElementById('box-ani');
+    box.style.backgroundColor = 'white';
+    box.style.border = '10px solid violet';
+    box.style.display= 'block';
+    setTimeout(function() {
+      box.style.transform = 'translateY(250px)';
+    }, 100);
+  });
+
+document.getElementById('wed').addEventListener('click', function() {
+    let box = document.getElementById('box-ani');
+    box.style.backgroundColor = 'white';
+    box.style.border = '10px solid blue';
+    box.style.display= 'block';
+    setTimeout(function() {
+      box.style.transform = 'translateY(250px)';
+    }, 100);
+  });
+
+document.getElementById('thurs').addEventListener('click', function() {
+    let box = document.getElementById('box-ani');
+    box.style.backgroundColor = 'white';
+    box.style.border = '10px solid greenyellow';
+    box.style.display= 'block';
+    setTimeout(function() {
+      box.style.transform = 'translateY(250px)';
+    }, 100);
+  });
+
+document.getElementById('fri').addEventListener('click', function() {
+    let box = document.getElementById('box-ani');
+    box.style.backgroundColor = 'white';
+    box.style.border = '10px solid yellow';
+    box.style.display= 'block';
+    setTimeout(function() {
+      box.style.transform = 'translateY(250px)';
+    }, 100);
+  });
+
+document.getElementById('sat').addEventListener('click', function() {
+    let box = document.getElementById('box-ani');
+    box.style.backgroundColor = 'white';
+    box.style.border = '10px solid orange';
+    box.style.display= 'block';
+    setTimeout(function() {
+      box.style.transform = 'translateY(250px)';
+    }, 100);
+  });
+
+document.getElementById('sun').addEventListener('click', function() {
+    let box = document.getElementById('box-ani');
+    box.style.backgroundColor = 'white';
+    box.style.border = '10px solid red';
+    box.style.display= 'block';
+    setTimeout(function() {
+      box.style.transform = 'translateY(250px)';
+    }, 100);
+  });
